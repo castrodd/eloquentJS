89a0724375d2fa921f75e9b618b2b42a520e0b98
Daniel Castro <castrodd@gmail.com>
2022-09-30 17:00:38 -0700

    prepend and nth

--- chapter-four/list.js
+++ chapter-four/list.js
@@ -32,5 +32,18 @@ const listToArray = (list) => {
   return array
 }
 
+const prepend = (element, list) => {
+  return {value: element, rest: list}
+}
+
+const nth = (index, list) => {
+  if (index) return nth(index - 1, list.rest)
+  return list.value
+}
+
 console.log(arrayToList([1, 2, 3]))
 console.log(listToArray({ value: 1, rest: { value: 2, rest: { value: 3, rest: null } } }))
+console.log(prepend(99, {value: 1, rest: { value: 2, rest: { value: 3, rest: null }}}))
+console.log(nth(2, { value: 1, rest: { value: 2, rest: { value: 3, rest: null } } }))
+console.log(nth(1, { value: 1, rest: { value: 2, rest: { value: 3, rest: null } } }))
+console.log(nth(0, { value: 1, rest: { value: 2, rest: { value: 3, rest: null } } }))
